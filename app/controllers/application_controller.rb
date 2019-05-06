1af0b1995653d340df83e0b542bd86742e0e8472
Sachin Mahajan <sachinm78@gmail.com>
2019-05-06 23:07:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 require './config/environment'
+require 'sinatra/base'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     enable :sessions
+    use Rack::Flash
     set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -35,24 +38,12 @@ class ApplicationController < Sinatra::Base
   helpers do
 
     def logged_in?
-      !!current_user
+      !!current_user    
     end
 
-    def login(username, password)
-      user = User.find_by(:username => username)
-      if  user && user.authenticate(password)
-    	session[:user_id] = user.id
-      else
-    	 redirect "/login"
-      end
-    end
 
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
-  
-    def log_out
-      session.destroy
     end
   end
 end
