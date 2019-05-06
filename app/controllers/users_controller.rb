d512b6f02ec8b34463cab178046de80545c077c1
Sachin Mahajan <sachinm78@gmail.com>
2019-05-06 23:47:56 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,20 @@ class UsersController < ApplicationController
   
   get '/signup' do
     if !logged_in?
-      erb :'users/create_user'
+       erb :'users/create_user'
     else
-      redirect to '/tweets'
+      redirect '/tweets'
     end
   end
 
   post '/signup' do
-    params.each do |label, input|
-      if input.empty?
-        flash[:new_user_error] = "Please enter a value for #{label}"
-        redirect to '/signup'
-      end
+    if params[:username] == '' || params[:email] =='' || params[:password] == ''
+      redirect '/signup'
+    else
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect '/tweets'
     end
-    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-    @user.save
-    session[:user_id] = @user.id
-    redirect to '/tweets'
   end
 
   get '/login' do
